431afefc6c044cc0ff5837d3531149c2cec7b1e2
liamrichardson <liampgrichardson@gmail.com>
2025-02-12 23:48:28 +0000

    updated main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -60,6 +60,11 @@ resource "aws_lb_listener" "http_listener" {
       on_unauthenticated_request = "authenticate"
     }
   }
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.my_target_group.arn
+  }
 }
 
 # VPC Creation
@@ -166,18 +171,6 @@ resource "aws_lb_target_group" "my_target_group" {
   protocol   = "HTTP"
   vpc_id     = aws_vpc.my_vpc.id
   target_type = "ip" # Required for Fargate tasks
-}
-
-# ALB Listener
-resource "aws_lb_listener" "http_listener" {
-  load_balancer_arn = aws_lb.my_alb.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.my_target_group.arn
-  }
 }
 
 # ECS Cluster Creation
